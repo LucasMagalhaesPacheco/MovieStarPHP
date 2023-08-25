6235d150763547d09c190454ef2b84607c281c40
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-08-25 16:43:01 -0300

    Área de autorização, templates separados

---
+++ auth.php
@@ -0,0 +1,58 @@
+<?php
+include_once("templates/header.php")
+
+?>
+<div id="main-container" class="container-fluid">
+    <div class="col-md-12">
+        <div class="row" id="auth-row">
+            <div class="col-md-4" id="login-container">
+                <h2>Entrar</h2>
+                <form action="" method="POST">
+                    <input type="hidden" name="type" value="login">
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Digie seu E-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digie sua senha">
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Entrar">
+                </form>
+            </div>
+            <div class="row" id="auth-row">
+                <div class="col-md-4" id="register-container">
+                    <h2>Criar Conta</h2>
+                    <form action="" method="POST">
+                        <input type="hidden" name="type" value="register">
+                        <div class="form-group">
+                            <label for="email">E-mail:</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Digie seu E-mail">
+                        </div> 
+                        <div class="form-group">
+                            <label for="name">name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Digie seu Nome">
+                        </div>
+                        <div class="form-group">
+                        <label for="lastname">sobrenome:</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Digie seu sobrenome">
+                        
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digie sua senha">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmPassword">Confirmação de senha:</label>
+                        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Confirme sua senha">
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Registrar">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include_once("templates/footer.php")
+?>
--- index.php
+++ index.php
@@ -1,83 +1,11 @@
-<?php
 
- require_once("globals.php");
- require_once("db.php")
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MovieStar</title>
-    <link rel="short icon" href="<?php echo $BASE_URL ?>img/movies/logo.png" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.css" integrity="sha512-azoUtNAvw/SpLTPr7Z7M+5BPWGOxXOqn3/pMnCxyDqOiQd4wLVEp0+AqV8HcoUaH02Lt+9/vyDxwxHojJOsYNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="<?php echo $BASE_URL ?>css/styles.css">
-
-</head>
-<body>
-    <header>
-        <nav id="main-navbar" class="navbar navbar-expand-lg">
-            <a href="<?php echo $BASE_URL ?>" class="navbar-brand">
-        <img src="<?php echo $BASE_URL ?>img/logo.png" alt="Moviestar Logo" id="logo">
-         <span id="moviestar-title">MovieStar</span>
-         </a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-        </button>
-         <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-         <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-        <button class="btn my-2 my-sm-0" type="submit">
-          <i class="fas fa-search"></i>
-        </button>
-         </form>
-         <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="<?php echo $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar </a>
-                </li>
-            </ul>
-         </div>
-        </nav>
-    </header>
+   <?php 
+    include_once("templates/header.php")
+   
+   ?>
     <div id="main-container" class="container-fluid">
         <h1>Corpo do site</h1>
     </div>
-    <footer id="footer">
-     <div id="social-container">
-        <ul>
-            <li>
-               <a href="#"><i class="fab fa-facebook-square"></i></a> 
-            </li>
-            <li>
-               <a href="#"><i class="fab fa-instagram"></i></a> 
-            </li>
-            <li>
-               <a href="#"><i class="fab fa-youtube"></i></a> 
-            </li>
-            <li>
-            </li>
-        </ul>
-     </div>
-     <div id="footer-links-container">
-        <ul>
-            <li>
-                <a href="#">Adicionar filmes</a>
-            </li>
-            <li>
-                <a href="#">Adicionar Crítica</a>
-            </li>
-            <li>
-                <a href="#">Entrar / Registrar</a>
-            </li>
-        </ul>
-     </div>
-     <p>&copy; 2023 Lucas Magalhães</p>
-    </footer>
- <!-- BOOTSTRAP JS -->
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.js" integrity="sha512-hidrIwTPV6f4zhtlHGC2IelVMrCqiHkHC9AuFJk22S95fMp5qnbhz2uLw5WH+lVWEWKg5nbRPkBsa4nNPGubsw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+   <?php 
+    include_once("templates/footer.php") 
+    ?>
